d1046a5a6d0a0465f6d24d4cb99e5ae69590b736
Philip Corliss <pcorliss@50projects.com>
2014-11-07 12:06:22 -0600

    Ditching mistaken obj_id

--- app.rb
+++ app.rb
@@ -3,5 +3,5 @@ require './lib/jokes'
 
 get '/joke.json' do
   @@jokes ||= Jokes.new
-  @@jokes.get_joke(params[:type]).to_hash.merge(obj_id: @jokes.object_id).to_json
+  @@jokes.get_joke(params[:type]).to_json
 end
