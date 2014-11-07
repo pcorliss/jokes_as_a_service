cda29a941bf2d737d1c2e1df95827c84050f410c
Philip Corliss <pcorliss@50projects.com>
2014-11-07 12:03:56 -0600

    Maintaing client connections between web requests

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 require './lib/jokes'
 
 get '/joke.json' do
-  @jokes ||= Jokes.new
-  @jokes.get_joke(params[:type]).to_json
+  @@jokes ||= Jokes.new
+  @@jokes.get_joke(params[:type]).to_hash.merge(obj_id: @jokes.object_id).to_json
 end
